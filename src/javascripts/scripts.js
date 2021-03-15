984867757c2c57dc819f7bee046a15e5223aefc8
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2021-03-15 23:47:16 +0200

    added 3 sections after HERO
    - added slick
    - added jquery

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,18 @@
 // Add your scripts here
+import '../../node_modules/slick-carousel/slick/slick.min';
+import $ from 'jquery';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slickElement = document.querySelector('.review__slick');
+
+  if (slickElement) {
+    $(slickElement).slick({
+      dots: false,
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      variableWidth: true,
+    });
+  }
+});
